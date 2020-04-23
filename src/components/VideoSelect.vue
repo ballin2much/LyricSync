5436563e59fd4326ddf8dde3046fc0e1a72471bf
Ballin2much <smomark12@gmail.com>
2020-04-22 23:11:48 -0400

    Added ability to store written n lyrics in a formatted TXT file.

--- src/components/VideoSelect.vue
+++ src/components/VideoSelect.vue
@@ -1,12 +1,17 @@
 <template>
     <div>
+      <div>
+        <input type="file" ref="myFile" @change="loadFile">
+      </div>
+      <div>
         <form @submit.prevent="loadVideo">  
-          <input type="text" name="Youtube Video" placeholder="Enter Youtube URL" v-model="YTURL">
+          <input type="text" name="YTurl" placeholder="Enter Youtube URL" v-model="YTurl">
           <input type="submit" value="Load Video" class="btn">
         </form>
-        <div v-for="ID in IDT" v-bind:key="ID">
-          <YTVideo :ytid="ID" />
-        </div>   
+      </div>
+      <div v-for="TID in IDT" v-bind:key="TID.ID">
+        <YTVideo :ytid="TID.ID" :lines="TID.Lines"/>
+      </div>   
     </div>
 </template>
 
@@ -19,22 +24,68 @@ export default {
         YTVideo
     },
     methods: {
-        loadVideo() {
-            let url = this.YTURL;
-            let ID = '';
-            url = url.replace(/(>|<)/gi,'').split(/(vi\/|v=|\/v\/|youtu\.be\/|\/embed\/)/);
-            if(url[2] !== undefined) {
-                ID = url[2].split(/[^0-9a-z_-]/i);
-                ID = ID[0];
-            } else {
-                ID = url;
-            }
-            this.IDT = [ID];
+      loadVideo(fileText) {
+        let url = this.YTurl;
+        let ID = '';
+        url = url.replace(/(>|<)/gi,'').split(/(vi\/|v=|\/v\/|youtu\.be\/|\/embed\/)/);
+        if(url[2] !== undefined) {
+          ID = url[2].split(/[^0-9a-z_-]/i);
+          ID = ID[0];
+        } else {
+          ID = url;
         }
+        ID = ID[0];
+        if (fileText == null) {
+          this.IDT = [{
+            ID: ID,
+            Lines: []
+          }];
+        } else {
+          this.IDT = [{
+            ID: ID,
+            Lines: fileText
+          }];
+        }
+        console.log("IDT:");
+        console.log(this.IDT);
+        this.YTurl = "";
+      },
+      loadFile() {
+        let file = this.$refs.myFile.files[0];
+        if(!file || file.type !== 'text/plain') return;
+        let reader = new FileReader();
+        reader.readAsText(file, "UTF-8");
+        reader.onload =  evt => {
+          let fileText = evt.target.result;
+          fileText = fileText.split("\n");
+          console.log(fileText);
+          this.YTurl = fileText.shift();
+          let lineArray = []
+          fileText.forEach( line => {
+            let totalSeconds = parseFloat(line.substring(line.lastIndexOf("[")+1,line.lastIndexOf("]")));
+            let temp = {
+              timeStamp: totalSeconds,
+              text: line.split("] ")[1],
+              formattedTime: () => {
+                let hours = Math.floor(totalSeconds / 3600);
+                totalSeconds %= 3600;
+                let minutes = Math.floor(totalSeconds / 60);
+                let seconds = Math.round(totalSeconds % 60);
+                return hours+":"+minutes+":"+seconds;
+              },
+              active: false
+            }
+            lineArray.push(temp);
+          });
+          console.log(lineArray);
+          this.loadVideo(lineArray);
+        }
+      }
     },
     data: () => {
         return {
-            IDT: []
+            IDT: [],
+            YTurl: ""
         }
     }
 }
--- src/components/YTVideo.vue
+++ src/components/YTVideo.vue
@@ -1,9 +1,12 @@
 <template>
     <div>
         <youtube :video-id="ytid" ref="youtube" @playing="refreshtime()"/>
+        <div>
+          <button @click="saveLyrics" type="button">Download Lyrics</button>
+        </div>
         <p>{{ ytid }}</p>
         <form @submit.prevent="addLine">  
-          <input type="text" name="line" placeholder="Add Lyrics..." v-model="line">
+          <input type="text" name="linetext" placeholder="Add Lyrics..." v-model="linetext">
           <input type="submit" value="Enter Lyrics" class="btn">
         </form>
         <LineList :lines="lines" v-on:changeTime="skipTo"/>       
@@ -16,14 +19,17 @@ let intID = null;
 
 export default {
     name: 'YTVideo',
-    data: () => {
-      return {lines: []};
-    },
-    components: {
-      LineList
-    },
     props: {
-        ytid: String
+        ytid: String,
+        lines: Array
+    },
+    data: () => {
+      return {
+        linetext: ""
+      }
+    },
+    components: { 
+      LineList
     },
     methods: {
         async getDuration() {
@@ -32,11 +38,18 @@ export default {
         async getTime() {
           return await this.$refs.youtube.player.getCurrentTime();
         },
+        saveLyrics() {
+          let text = [this.ytid]
+          this.lines.forEach(line => text.push("\n[" + line.timeStamp + "] " + line.text))
+          let FileSaver = require('file-saver');
+          let blob = new Blob(text, {type: "text/plain;charset=utf-8"});
+          FileSaver.saveAs(blob, "Lyrics");
+        },
         async addLine() {
           let totalSeconds = await this.getTime();
           const newLine = {
             timeStamp: totalSeconds,
-            text: this.line,
+            text: this.linetext,
             formattedTime: () => {
               let hours = Math.floor(totalSeconds / 3600);
               totalSeconds %= 3600;
@@ -47,10 +60,19 @@ export default {
             active: true
           }
           this.addTodo(newLine);
+          this.linetext = "";
         },
         addTodo(newLine) {
-          this.lines.forEach(line => line.active = false);
-          this.lines = [...this.lines, newLine];
+          let x = this.lines.length + 1;
+          let boo = true;
+          for (let i = 0; i < this.lines.length; i++) {
+            this.lines[i].active = false; 
+            if (this.lines[i].timeStamp > newLine.timeStamp && boo) {
+              x = i;
+              boo = false;
+            } 
+          }
+          this.lines = this.lines.splice(x, 0, newLine)
         },
         async skipTo(lineI) {
           this.lines.forEach(line => line.active = false);
@@ -63,11 +85,7 @@ export default {
             let time = await this.$refs.youtube.player.getCurrentTime();
             let temp = null;
             this.lines.forEach((line) => {
-              console.log("Time:" + time);
-              console.log("Line:" + line.timeStamp);
-              if (temp == null) console.log("Temp:" + temp);
-              if (temp != null) console.log("Temp:" + temp.timeStamp);
-            line.active = false;
+              line.active = false;
               if (temp == null) {
                 if (line.timeStamp < time) {
                   temp = line;
